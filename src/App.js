ff234c793cd2c72945938ebce8095299be4472a8
Dang Viet Cuong <cuong11101997@gmail.com>
2021-03-25 15:35:24 +0700

    done setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
+
+  
   return (
     <Router>
       <Route path='/'>
         <div>
-        
+            <Button type='primary' size="small">Loading</Button>
         </div>
       </Route>
     </Router>
